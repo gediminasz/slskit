0f04193c718643003e07501b3833c5dcc8300d6b
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-08-05 22:43:30 +0300

    Add multiple pillar assignment along with nested pillars

--- salinity/__main__.py
+++ salinity/__main__.py
@@ -13,6 +13,11 @@ with top_file.open() as f:
 def matches(minion_id, selector):
     return (selector == "*") or (minion_id in selector)
 
+def load_pillar(pillar_id):
+    path = root_path.joinpath(*pillar_id.split(".")).with_suffix(".sls")
+    with path.open() as f:
+        return yaml.safe_load(f)
+
 minions = {
     "stuart": {},
     "kevin": {},
@@ -20,12 +25,11 @@ minions = {
 }
 
 for selector, pillars in top["base"].items():
-    for pillar in pillars:
-        with (root_path / pillar).with_suffix(".sls").open() as f:
-            data = yaml.safe_load(f)
+    for pillar_id in pillars:
+        data = load_pillar(pillar_id)
 
-    for minion_id, minion_data in minions.items():
-        if matches(minion_id, selector):
-            minion_data.update(data)
+        for minion_id, minion_data in minions.items():
+            if matches(minion_id, selector):
+                minion_data.update(data)
 
 pprint(minions)
