73537b6b511818ff7cbc785577457882199b2198
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-08-05 22:53:37 +0300

    Reuse load_pillar

--- salinity/__main__.py
+++ salinity/__main__.py
@@ -4,16 +4,10 @@ import os
 
 import yaml
 
-root_path = Path("pillar")
-top_file = root_path / "top.sls"
-
-with top_file.open() as f:
-    top = yaml.safe_load(f)
-
 def matches(minion_id, selector):
     return (selector == "*") or (minion_id in selector)
 
-def load_pillar(pillar_id):
+def load_pillar(root_path, pillar_id):
     path = root_path.joinpath(*pillar_id.split("."))
     if path.with_suffix(".sls").exists():
         return load_yaml(path.with_suffix(".sls"))
@@ -23,15 +17,18 @@ def load_yaml(path):
     with path.open() as f:
         return yaml.safe_load(f)
 
+root_path = Path("pillar")
+top_file = root_path / "top.sls"
+
 minions = {
     "stuart": {},
     "kevin": {},
     "bob": {},
 }
 
-for selector, pillars in top["base"].items():
+for selector, pillars in load_yaml(top_file)["base"].items():
     for pillar_id in pillars:
-        data = load_pillar(pillar_id)
+        data = load_pillar(root_path, pillar_id)
 
         for minion_id, minion_data in minions.items():
             if matches(minion_id, selector):
