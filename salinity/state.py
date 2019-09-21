087a68b7d0862ddbd8cf6c5c7f5a72c4a605ee22
Gediminas Zlatkus <gediminas.zlatkus@oxipit.ai>
2019-09-21 15:04:08 +0300

    Remove salt mock from state.top

--- salinity/state.py
+++ salinity/state.py
@@ -1,10 +1,10 @@
-from salinity.mocks import MockDict, PrettyMock
+from salinity.mocks import PrettyMock
 from salinity.renderer import Renderer
 from salinity.top import Top
 
 
 def top(args):
-    context = {"pillar": PrettyMock(name="PILLAR"), "salt": MockDict()}
+    context = {"pillar": PrettyMock(name="PILLAR")}
     renderer = Renderer(args.state_root, context)
     top = Top.load(args.state_root, args.state_top, renderer)
     return top.body
