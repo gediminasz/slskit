96100d360ff2566aa32cdc0a6023d4e3b4c765ad
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-01-02 21:05:15 +0200

    Display rendering errors

--- salinity/__init__.py
+++ salinity/__init__.py
@@ -1,5 +1,14 @@
+import logging
 import runpy
+
+import colorlog
 
 
 def run_module():
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter("%(log_color)s%(levelname)s:%(name)s:%(message)s")
+    )
+    logging.basicConfig(level=logging.WARNING, handlers=(handler,))
+
     runpy.run_module("salinity")
