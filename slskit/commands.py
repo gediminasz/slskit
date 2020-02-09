d04cff4618baedeb6357fce085f15ebf0196e9b3
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-02-09 14:25:00 +0200

    Halt snapshot commands if rendering fails

--- slskit/commands.py
+++ slskit/commands.py
@@ -1,6 +1,7 @@
 import difflib
 import sys
 from pathlib import Path
+from typing import Optional
 from unittest.mock import patch
 
 import salt.output
@@ -37,25 +38,35 @@ def refresh(config: Config):
 
 def create_snapshot(config: Config):
     dump = _dump_highstate(config)
+    if not dump:
+        sys.exit("Failed to render snapshot")
+
     config.snapshot_path.write_text(dump)
+    print(f"Snapshot saved as `{config.snapshot_path}`")
 
 
 def check_snapshot(config: Config):
     if not config.snapshot_path.exists():
-        sys.exit(f"Snapshot file {config.snapshot_path} not found")
-
+        sys.exit(f"Snapshot file `{config.snapshot_path}` not found")
     snapshot = config.snapshot_path.read_text()
-    current = _dump_highstate(config)
 
-    if snapshot != current:
-        _display_diff(snapshot, current)
-        sys.exit(1)
+    dump = _dump_highstate(config)
+    if not dump:
+        sys.exit("Failed to render snapshot")
+
+    if snapshot != dump:
+        _display_diff(snapshot, dump)
+        sys.exit(
+            "There are some changes not present in the snapshot. "
+            "Run `slskit snapshot create` to update the snapshot."
+        )
 
 
-def _dump_highstate(config: Config) -> str:
+def _dump_highstate(config: Config) -> Optional[str]:
     highstate = state.show_highstate(config)
-    snapshot = {minion_id: result.value for minion_id, result in highstate.items()}
-    return salt.utils.yaml.safe_dump(snapshot, default_flow_style=False)
+    if all(r.valid for r in highstate.values()):
+        snapshot = {minion_id: result.value for minion_id, result in highstate.items()}
+        return salt.utils.yaml.safe_dump(snapshot, default_flow_style=False)
 
 
 def _display_diff(a: str, b: str):
