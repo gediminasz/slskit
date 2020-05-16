1e78ebfc6754fa75de0c639bfc1a005f9d34340c
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2020-05-16 12:34:04 +0300

    Reduce duplication in output and validation code

--- slskit/pillar.py
+++ slskit/pillar.py
@@ -4,18 +4,22 @@ import salt.output
 import salt.pillar
 
 from .opts import Config
-from .types import AnyDict
+from .types import AnyDict, MinionDict, Result
 
 
-def items(config: Config) -> Dict[str, AnyDict]:
-    return {
-        minion_id: compile_pillar(
-            opts=config.opts, grains=config.grains_for(minion_id), minion_id=minion_id
-        )
-        for minion_id in config.minion_ids
-    }
+def items(config: Config) -> MinionDict:
+    return MinionDict(
+        {
+            minion_id: compile_pillar(
+                opts=config.opts,
+                grains=config.grains_for(minion_id),
+                minion_id=minion_id,
+            )
+            for minion_id in config.minion_ids
+        }
+    )
 
 
-def compile_pillar(opts: AnyDict, grains: AnyDict, minion_id: str) -> AnyDict:
+def compile_pillar(opts: AnyDict, grains: AnyDict, minion_id: str) -> Result:
     pillar = salt.pillar.get_pillar(opts, grains, minion_id).compile_pillar()
-    return cast(AnyDict, pillar)
+    return Result(valid="_errors" in pillar, value=pillar)
--- slskit/types.py
+++ slskit/types.py
@@ -1,3 +1,20 @@
+from dataclasses import dataclass
 from typing import Any, Dict
 
 AnyDict = Dict[str, Any]
+
+
+@dataclass(frozen=True)
+class Result:
+    valid: bool
+    value: Any
+
+
+class MinionDict(Dict[str, Result]):
+    @property
+    def all_valid(self) -> bool:
+        return all(result.valid for result in self.values())
+
+    @property
+    def output(self) -> AnyDict:
+        return {minion_id: result.value for minion_id, result in self.items()}
