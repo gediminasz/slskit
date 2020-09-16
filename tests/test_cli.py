969ccab5cf7d33d644a0f46a6cc0c716d4869f1a
Gediminas Zlatkus <gediminas.zlatkus@oxipit.ai>
2020-09-16 21:40:03 +0300

    Add snapshot tests for the rest of CLI commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,21 @@
 import subprocess
 
+import pytest
+
 
 def execute(command):
-    return subprocess.run(command.split(), capture_output=True).stdout.decode()
+    return
 
 
-def test_template(snapshot):
-    snapshot.assert_match(
-        execute(
-            "poetry run slskit template tests/project/salt/template/child.txt tester"
-        ),
-        "template.snap",
-    )
+@pytest.mark.parametrize(
+    "command",
+    (
+        "poetry run slskit highstate",
+        "poetry run slskit sls detached",
+        "poetry run slskit pillars",
+        "poetry run slskit template tests/project/salt/template/child.txt tester",
+    ),
+)
+def test_command_output_snapshot(command, snapshot):
+    output = subprocess.run(command.split(), capture_output=True).stdout.decode()
+    snapshot.assert_match(output, "output.snap")
