ba0213fb6c8d979058b982fcdb097f470b463f02
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2021-03-03 23:41:31 +0200

    Fix linter warnings

--- slskit/lib/cli.py
+++ slskit/lib/cli.py
@@ -6,11 +6,11 @@ log = logging.getLogger("slskit")
 
 
 def minion_id_argument():
-    def callback(ctx, param, value):
+    def callback(ctx, _, value):
         value = value or ctx.obj["config"].roster.keys()
         unknown_ids = set(value) - set(ctx.obj["config"].roster.keys())
         if unknown_ids:
-            log.warning(f"Minion ids not found in roster: {', '.join(unknown_ids)}")
+            log.warning("Minion ids not found in roster: %s", ", ".join(unknown_ids))
         return value
 
     return click.argument("minion_id", nargs=-1, callback=callback)
