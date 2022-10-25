e5f5f419c87fcdb6d7ae472c20719222ea2fd434
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2022-10-25 23:36:39 +0300

    Remove basicConfig(..., force=True) workaroung

--- slskit/lib/logging.py
+++ slskit/lib/logging.py
@@ -1,20 +1,7 @@
 import logging
-from typing import Any, Iterable
-
-import colorlog
+from typing import Iterable
 
 LEVEL_CHOICES = tuple(logging._nameToLevel.keys())
-
-
-def basic_config(**kwargs: Any) -> None:
-    # borrowed `force` implementation from Python 3.8
-    # https://github.com/python/cpython/blob/v3.8.0/Lib/logging/__init__.py#L1954-L1958
-    # TODO use basicConfig(..., force=True) after dropping support for Python 3.7
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
-        handler.close()
-
-    colorlog.basicConfig(**kwargs)
 
 
 def log_errors(title: str, errors: Iterable[str]) -> None:
