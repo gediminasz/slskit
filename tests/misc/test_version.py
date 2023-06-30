6f80b57210468cb937817a8c70b6aada1bd3f369
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2023-06-30 18:47:46 +0300

    Use tomllib

--- tests/misc/test_version.py
+++ tests/misc/test_version.py
@@ -1,9 +1,12 @@
-import tomli
+try:
+    import tomllib
+except ImportError:  # TODO remove this workaround when Python 3.10 support is dropped
+    import tomli as tomllib
 
 import slskit
 
 
 def test_version_in_module_matches_pyproject():
     with open("pyproject.toml", "rb") as project_file:
-        project = tomli.load(project_file)
+        project = tomllib.load(project_file)
     assert slskit.__version__ == project["tool"]["poetry"]["version"]
